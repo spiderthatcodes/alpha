07497fdc6e15c5c7e825edfc3f8877d744dc5d98
spiderthatcodes <jessicaldickerson@outlook.com>
2023-09-05 13:48:05 -0700

    Feature 14 complete

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Project
+from .forms import ProjectForm
 
 
 @login_required(login_url='/accounts/login/')
@@ -21,3 +22,22 @@ def show_project(request, id):
     }
 
     return render(request, "projects/details.html", context)
+
+
+@login_required(login_url='/accounts/login/')
+def create_project(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(False)
+            project.owner = request.user
+            project.save()
+            return redirect("list_projects")
+    else:
+        form = ProjectForm()
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "projects/create.html", context)
