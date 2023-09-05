6f95fd2331f44fef11d7eb3f1cac9ba4c01d95b6
spiderthatcodes <jessicaldickerson@outlook.com>
2023-09-05 13:17:46 -0700

    Feature 12 complete

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "id"
+    )
